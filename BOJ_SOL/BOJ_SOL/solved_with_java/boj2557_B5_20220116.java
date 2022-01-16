91cec2f7cf1b4a763b508615ab074e0f08de41e4
이유나 <=>
2022-01-16 17:29:49 +0900

    feat: solved boj2557

---
+++ BOJ_SOL/BOJ_SOL/solved_with_java/boj2557_B5_20220116.java
@@ -0,0 +1,10 @@
+package BOJ_SOL.BOJ_SOL.solved_with_java;
+
+/**
+ * Hello World! 출력하기
+ */
+public class boj2557_B5_20220116 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
